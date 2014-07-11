98e181cf1701a18af0d4aebe7be20fc5ca4cdbb2
Luca Cervasio <luca.cervasio@gmail.com>
2014-07-11 15:27:48 +0200

    added new api for cpe sim

--- lib/cwmp/cpe.rb
+++ lib/cwmp/cpe.rb
@@ -4,21 +4,56 @@ module Cwmp
 
     class Cpe
 
-        attr_accessor :serial, :oui, :software_version, :manufacturer, :state, :acs_url
+        attr_accessor :serial, :oui, :software_version, :manufacturer, :acs_url, :state
 
-        def initialize (serial, oui, software_version, manufacturer, acs_url)
-            @serial = serial
-            @oui = oui
-            @software_version = software_version
-            @manufacturer = manufacturer
-            @state = '0 BOOTSTRAP'
+        def initialize (acs_url, h = {})
             @acs_url = acs_url
+            @serial = h[:serial] || "23434ds"
+            @oui = h[:oui] || "0013C8"
+            @software_version = h[:software_version] || "0.1.1"
+            @manufacturer = h[:manufacturer] || "Moonar"
+            @state = 'off'
+            @t = Thread.new { periodic 10 }
+            @t = Thread.new { connection_request }
         end
 
-        def start
+        def poweron
+            @state = 'on'
+            do_connection '0 BOOTSTRAP'
+        end
+
+        def poweroff
+            @state = 'off'
+        end
+
+        def reboot
+            @state = 'off'
+            sleep 2
+            @state = 'on'
+            do_connection '1 BOOT'
+        end
+
+        def reset
+            do_connection '0 BOOTSTRAP'
+        end
+
+        private
+
+        def periodic time
+            while true
+                sleep time
+                do_connection '2 PERIODIC'
+            end
+        end
+
+        def connection_request
+
+        end
+
+        def do_connection(event)
             c = HTTPClient.new
 
-            resp = c.post @acs_url, Cwmp::Message::inform(@manufacturer, @oui, @serial, @state, @software_version)
+            resp = c.post @acs_url, Cwmp::Message::inform(@manufacturer, @oui, @serial, event, @software_version)
             doc = Nokogiri::XML(resp.body)
             message_type = doc.css("soap|Body").children.map(&:name)[1]
             puts "got #{message_type} message"
@@ -36,10 +71,10 @@ module Cwmp
                         resp = c.post @acs_url, Cwmp::Message::get_parameter_names_response
                     when "SetParameterValues"
                         puts "got #{message_type}"
+                        resp = c.post @acs_url, Cwmp::Message::set_parameter_values_response
                 end
             end
             puts "got #{resp.status}, closing"
-
         end
 
 
