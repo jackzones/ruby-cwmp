151c408604ef1da9c507fe9c10d822fd790fdc32
Luca Cervasio <luca.cervasio@gmail.com>
2014-07-14 17:24:11 +0200

    cpe accepts periodic interval parameter

--- lib/cwmp/cpe.rb
+++ lib/cwmp/cpe.rb
@@ -4,17 +4,16 @@ require 'socket'
 module Cwmp
 
     class Cpe
-
         attr_accessor :serial, :oui, :software_version, :manufacturer, :acs_url, :state
 
-        def initialize (acs_url, h = {})
+        def initialize (acs_url, periodic, h = {})
             @acs_url = acs_url
             @serial = h[:serial] || "23434ds"
             @oui = h[:oui] || "006754"
             @software_version = h[:software_version] || "0.1.1"
             @manufacturer = h[:manufacturer] || "Moonar"
             @state = 'off'
-            @periodic = Thread.new { periodic 30 }
+            @periodic = Thread.new { periodic periodic } if periodic > 0
             @conn_req = Thread.new { connection_request }
             @factory = true
         end
@@ -47,12 +46,13 @@ module Cwmp
         end
 
         def loop
-            @periodic.join
+            @conn_req.join
         end
 
         private
 
         def periodic time
+            puts "setting periodic inform every #{time} seconds"
             while true
                 sleep time
                 do_connection '2 PERIODIC'
@@ -80,32 +80,37 @@ module Cwmp
         end
 
         def do_connection(event)
-            c = HTTPClient.new
+            begin
+                c = HTTPClient.new
 
-            resp = c.post @acs_url, Cwmp::Message::inform(@manufacturer, @oui, @serial, event, @software_version)
-            puts "sending Inform with event #{event}"
-            doc = Nokogiri::XML(resp.body)
-            message_type = doc.css("Body").children.map(&:name)[1]
-            puts "got #{message_type} message"
-
-            resp = c.post @acs_url, ""
-            while resp.status != 204
+                puts "sending Inform with event #{event}"
+                resp = c.post @acs_url, Cwmp::Message::inform(@manufacturer, @oui, @serial, event, @software_version), {'Server' => "ruby-cwmp #{Cwmp::VERSION}"}
                 doc = Nokogiri::XML(resp.body)
                 message_type = doc.css("Body").children.map(&:name)[1]
-                case message_type
-                    when "GetParameterValues"
-                        puts "got #{message_type}"
-                        resp = c.post @acs_url, Cwmp::Message::get_parameter_values_response
-                    when "GetParameterNames"
-                        puts "got #{message_type}"
-                        resp = c.post @acs_url, Cwmp::Message::get_parameter_names_response
-                    when "SetParameterValues"
-                        puts "got #{message_type}"
-                        resp = c.post @acs_url, Cwmp::Message::set_parameter_values_response
+                puts "got #{message_type} message"
+
+                resp = c.post @acs_url, "", {'Server' => "ruby-cwmp #{Cwmp::VERSION}"}
+                while resp.status != 204
+                    doc = Nokogiri::XML(resp.body)
+                    message_type = doc.css("Body").children.map(&:name)[1]
+                    case message_type
+                        when "GetParameterValues"
+                            puts "got #{message_type}"
+                            resp = c.post @acs_url, Cwmp::Message::get_parameter_values_response
+                        when "GetParameterNames"
+                            puts "got #{message_type}"
+                            resp = c.post @acs_url, Cwmp::Message::get_parameter_names_response
+                        when "SetParameterValues"
+                            puts "got #{message_type}"
+                            resp = c.post @acs_url, Cwmp::Message::set_parameter_values_response
+                    end
                 end
+                puts "got #{resp.status}, closing"
+                c.reset @acs_url
+            rescue Errno::ECONNREFUSED
+                puts "can't connect to #{@acs_url}"
+                exit(1)
             end
-            puts "got #{resp.status}, closing"
-            c.reset @acs_url
         end
 
     end
