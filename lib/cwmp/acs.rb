86a3c21ccc36dd3a6c7e711082f4a7e6d291fa85
Luca Cervasio <luca.cervasio@gmail.com>
2015-08-18 08:50:57 +0200

    begin/rescue when running thin handler

--- lib/cwmp/acs.rb
+++ lib/cwmp/acs.rb
@@ -50,78 +50,83 @@ module Cwmp
         def call(env)
             req = Rack::Request.new(env)
             len = req.content_length.to_i
+            begin
 
-            if len == 0
-                message_type = ''
-            else
-                body = req.body.read
-                mess = Cwmp::Message::BaseMessage::parse_from_text body
-                # doc = Nokogiri::XML(body)
-                # body =~ /(\w+):Envelope/
-                # soap_ns = $1
-                message_type = mess.message_type
-            end
-
-            if message_type == "Inform"
-                manufacturer = doc.css("Manufacturer").text
-                oui = doc.css("OUI").text
-                serial_number = doc.css("SerialNumber").text
-                event_codes = doc.css("EventCode").map { |n| n.text }
-                parameters = {}
-                doc.css("ParameterValueStruct").map { |it| parameters[it.element_children[0].text] = it.element_children[1].text }
-
-                ck = Digest::MD5.hexdigest("#{serial_number}#{Time.new.to_i}")
-
-                if !@acs.cpes.has_key? serial_number
-                    cpe = AcsCpe.new serial_number
-                    cpe.conn_req = parameters['InternetGatewayDevice.ManagementServer.ConnectionRequestURL']
-                    cpe.session_cookie = ck
-                    @acs.cpes[serial_number] = cpe
-                end
-                puts "got Inform from #{req.ip}:#{req.port} [sn #{serial_number}] with eventcodes #{event_codes.join(", ")}"
-
-                inform_response = Cwmp::Message::inform_response
-                response = Rack::Response.new inform_response, 200, {'Connection' => 'Keep-Alive', 'Server' => 'ruby-cwmp'}
-                response.set_cookie("sessiontrack", {:value => ck, :path => "/", :expires => Time.now+24*60*60})
-                response.finish
-            elsif message_type == "TransferComplete"
-                puts "got TransferComplete"
-            else
-                cookie = req.cookies['sessiontrack']
-                @acs.cpes.each do |k,c|
-                    cpe = c if c.session_cookie = cookie
-                end
 
                 if len == 0
-                    puts "got Empty Post"
+                    message_type = ''
                 else
-                    puts "got #{message_type}"
-                    case message_type
-                        when "GetParameterValuesResponse"
-                            doc.css("ParameterValueStruct").each do |node|
-                                puts "#{node.element_children[0].text}: #{node.element_children[1].text}"
-                            end
-                        when "Fault"
-                            puts "#{doc.css("faultstring").text}: #{doc.css("FaultString").text}"
-                    end
-
-                    if cpe.req_currently_in_service != nil
-                        cpe.req_currently_in_service.cb.call(Cwmp::Message::BaseMessage.parse_from_text(body))
-                    end
+                    body = req.body.read
+                    mess = Cwmp::Message::parse_from_text body
+                    message_type = mess.message_type
                 end
 
+                puts message_type
+                if message_type == "Inform"
+                    doc = mess.parsed_xml_doc
+                    manufacturer = doc.css("Manufacturer").text
+                    oui = doc.css("OUI").text
+                    serial_number = doc.css("SerialNumber").text
+                    event_codes = doc.css("EventCode").map { |n| n.text }
+                    parameters = {}
+                    doc.css("ParameterValueStruct").map { |it| parameters[it.element_children[0].text] = it.element_children[1].text }
 
-                # Got Empty Post or a Response. Now check for any event to send, otherwise 204
-                if cpe.queue.size > 0
-                    m = cpe.queue.pop
-                    cpe.req_currently_in_service = m
-                    response = Rack::Response.new m.message, 200, {'Connection' => 'Keep-Alive', 'Server' => 'ruby-cwmp'}
+                    ck = Digest::MD5.hexdigest("#{serial_number}#{Time.new.to_i}")
+
+                    if !@acs.cpes.has_key? serial_number
+                        cpe = AcsCpe.new serial_number
+                        cpe.conn_req = parameters['InternetGatewayDevice.ManagementServer.ConnectionRequestURL']
+                        cpe.session_cookie = ck
+                        @acs.cpes[serial_number] = cpe
+                    end
+                    puts "got Inform from #{req.ip}:#{req.port} [sn #{serial_number}] with eventcodes #{event_codes.join(", ")}"
+
+                    inform_response = Cwmp::Message::InformResponse.build
+                    response = Rack::Response.new inform_response.to_s, 200, {'Connection' => 'Keep-Alive', 'Server' => 'ruby-cwmp'}
+                    response.set_cookie("sessiontrack", {:value => ck, :path => "/", :expires => Time.now+24*60*60})
                     response.finish
+                elsif message_type == "TransferComplete"
+                    puts "got TransferComplete"
                 else
-                    puts "sending 204"
-                    [204, {"Connection" => "Close", 'Server' => 'ruby-cwmp'}, ""]
-                end
+                    cookie = req.cookies['sessiontrack']
+                    @acs.cpes.each do |k,c|
+                        cpe = c if c.session_cookie = cookie
+                    end
 
+                    if len == 0
+                        puts "got Empty Post"
+                    else
+                        puts "got #{message_type}"
+                        case message_type
+                            when "GetParameterValuesResponse"
+                                doc.css("ParameterValueStruct").each do |node|
+                                    puts "#{node.element_children[0].text}: #{node.element_children[1].text}"
+                                end
+                            when "Fault"
+                                puts "#{doc.css("faultstring").text}: #{doc.css("FaultString").text}"
+                        end
+
+                        if cpe.req_currently_in_service != nil
+                            cpe.req_currently_in_service.cb.call(Cwmp::Message::BaseMessage.parse_from_text(body))
+                        end
+                    end
+
+
+                    # Got Empty Post or a Response. Now check for any event to send, otherwise 204
+                    if cpe.queue.size > 0
+                        m = cpe.queue.pop
+                        cpe.req_currently_in_service = m
+                        response = Rack::Response.new m.message, 200, {'Connection' => 'Keep-Alive', 'Server' => 'ruby-cwmp'}
+                        response.finish
+                    else
+                        puts "sending 204"
+                        [204, {"Connection" => "Close", 'Server' => 'ruby-cwmp'}, ""]
+                    end
+                end
+            rescue Exception => e
+                puts e
+                puts e.backtrace
+                raise e
             end
         end
 
@@ -143,14 +148,6 @@ module Cwmp
             end
             @cpes = {}
         end
-
-        # def GetParameterValues serial, leaf
-        #     cpe = @cpes[serial]
-        #     cpe.queue << Cwmp::Request.new(Cwmp::Message::GetParameterValues.build(leaf)) do |resp|
-        #         puts "arrived #{resp}"
-        #     end
-        #     cpe.do_connection_request
-        # end
 
         def start_cli
             list = [
