f0d90c79125c354c015069ccc49fcda4f78fb2b4
Luca Cervasio <luca.cervasio@gmail.com>
2015-08-18 08:50:32 +0200

    removed parser objetct

--- lib/cwmp.rb
+++ lib/cwmp.rb
@@ -1,6 +1,5 @@
 $:.unshift File.join(File.dirname(__FILE__), "cwmp")
 require 'acs'
 require 'message'
-require 'parser'
 require 'cpe'
 require 'version'
--- lib/cwmp/message/base_message.rb
+++ lib/cwmp/message/base_message.rb
@@ -1,35 +1,21 @@
 require 'nokogiri'
 
 module Cwmp
-    class MessageParser
+    module Message
 
-        def initialize msg
-            @msg = msg
-        end
+        NAMESPACES = {"xmlns:soap" => "http://schemas.xmlsoap.org/soap/envelope/", "xmlns:soap-enc" => "http://schemas.xmlsoap.org/soap/encoding/", "xmlns:cwmp" => "urn:dslforum-org:cwmp-1-0", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xml:xsd" => "http://www.w3.org/2001/XMLSchema", "xmlns" => "urn:dslforum-org:cwmp-1-0"}
 
-        def parse
-            doc = Nokogiri::XML(@msg)
-            @msg =~ /(\w+):Envelope/
+        def self.parse_from_text txtmsg
+            doc = Nokogiri::XML(txtmsg)
+            txtmsg =~ /(\w+):Envelope/
             soap_ns = $1
             message_type = doc.css("#{soap_ns}|Body")[0].element_children[0].name
 
             m = Object.const_get("Cwmp").const_get("Message").const_get(message_type).new # creating class from classname variable
             m.message_type = message_type
             m.parsed_xml_doc = doc
-            m.raw_xml_message = @msg
+            m.raw_xml_message = txtmsg
             return m
-        end
-
-    end
-
-    module Message
-
-        NAMESPACES = {"xmlns:soap" => "http://schemas.xmlsoap.org/soap/envelope/", "xmlns:soap-enc" => "http://schemas.xmlsoap.org/soap/encoding/", "xmlns:cwmp" => "urn:dslforum-org:cwmp-1-0", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xml:xsd" => "http://www.w3.org/2001/XMLSchema", "xmlns" => "urn:dslforum-org:cwmp-1-0"}
-
-        # call this way: Cwmp::Message::parse_from_text(msg)
-        def self.parse_from_text txtmsg
-            parser = Cwmp::MessageParser.new txtmsg
-            return parser.parse
         end
 
         class BaseMessage
